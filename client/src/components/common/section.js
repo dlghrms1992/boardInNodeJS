151784ce60850ded51034c50a7e238c58fac31d2
dev_G <dlghrms1992@naver.com>
2022-02-17 18:58:52 +0900

    부트스트랩  추가

--- client/src/components/common/section.js
+++ client/src/components/common/section.js
@@ -6,29 +6,35 @@ export default class Section extends Component {
     render(){
             return  (
                 <section className="section">
-                    <div className="container">
-                        <div className="contents">
-                            <div className="best">
-                                <div className="title">베스트</div>  
-                                <div className="category-list"></div>
+                    <div className="main">
+                        <div className="container">
+                            <div className="contents">
+                                <div className="best">
+                                    <div className="title">
+                                        <div className="text"><span>베스트</span></div>
+                                        <div className="more-category"><span>더보기</span></div>
+                                    </div>  
+                                    <div className="category-list"></div>
+                                </div>
+                                <Category/>
                             </div>
-                            <Category/>
+                            <div className="sub_container">
+                                <div className="visitors">
+                                    <div className="title">방문자</div>  
+                                    <div className="visitors-list"></div>
+                                </div>
+                                <div className="rank">
+                                    <div className="title">랭크</div>  
+                                    <div className="visitors-list"></div>
+                                </div>
+                                <div className="advertise">
+                                    <div className="title">광고</div>  
+                                    <div className="rank-list"></div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="sub_container">
-                            <div className="visitors">
-                                <div className="title">방문자</div>  
-                                <div className="visitors-list"></div>
-                            </div>
-                            <div className="rank">
-                                <div className="title">랭크</div>  
-                                <div className="visitors-list"></div>
-                            </div>
-                            <div className="advertise">
-                                <div className="title">광고</div>  
-                                <div className="rank-list"></div>
-                            </div>
-                         </div>
                     </div>
+       
                 </section>
             )
     }
