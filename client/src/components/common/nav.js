9b86c20fa7191774438e22a8efb7ab2e52d97da4
dev_G <dlghrms1992@naver.com>
2022-02-21 12:54:49 +0900

    모달 진행중

--- client/src/components/common/nav.js
+++ client/src/components/common/nav.js
@@ -6,16 +6,21 @@ export default class Nav extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            home : "",
-            write : "",
-            login : ""
+            homeOn: true,
+            writeOn : true,
+            loginOn : true
         };
         this.homeClick = this.homeClick.bind(this);
         this.writeClick = this.writeClick.bind(this);
         this.loginClick = this.loginClick.bind(this);
+        this.searchClick = this.searchClick.bind(this);
     }
 
     homeClick(){
+
+    }
+
+    searchClick(){
 
     }
 
@@ -24,31 +29,31 @@ export default class Nav extends Component {
     }
 
     loginClick(){
-
+        console.log("modal 띄우기전 테스트 입니다.");
     }
 
     render(){
-        const home = this.state.home;
-        const search = this.state.search;
-        const write = this.state.write;
-        const login = this.state.login;
+        // const home = this.state.home;
+        // const search = this.state.search;
+        // const write = this.state.write;
+        // const login = this.state.login;
         return (
             <nav className="nav">
                 <div className="top">
                     <div className="logo">
-                        <div className="logo" onClick={home}>정보공유</div>
-                        <a href="#" className="home" onClick={home}>HOME</a>
+                        <div className="logo" onClick={this.homeClick}>정보공유</div>
+                        <a href="#" className="home" onClick={this.homeClick}>HOME</a>
                     </div>
                     <div className="menuBar">
                         <div className="subMenu">
                             <Form.Control type="text" className="searchInput" placeholder="검색어를 입력해주세요!" />
-                            <Button className="search" variant="success" onClick={search}>검색</Button>
+                            <Button className="search" variant="success" onClick={this.searchClick}>검색</Button>
                         </div>
                         <div className="subMenu">
-                            <Button className="write" variant="danger" onClick={write}>글쓰기</Button>
+                            <Button className="write" variant="danger" onClick={this.writeClick}>글쓰기</Button>
                         </div>
                         <div className="subMenu">
-                            <Button  className="login" onClick={login}>로그인</Button>
+                            <Button  className="login" onClick={this.loginClick}>로그인</Button>
                         </div>
                     </div>
                 </div>
---
+++ client/src/components/modal/loginModal.js
@@ -0,0 +1,39 @@
+import React, {Component,useState} from 'react';
+import {Modal,Button, Form} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './header.js';
+import Footer from './footer.js';
+import Section from './section.js';
+
+
+export default class LoginModal extends Comment {
+    render(){
+        // const {login, register, close} = this.props;
+        return (
+        <div className="wrapper">
+            <Header/>
+                <Section>
+                    <div className="longinContainer">
+                        <div className="loginInput">
+                        <Form.Control type="text" className="loginId" placeholder="아이디를 입력해주세요!" />
+                        <Form.Control type="text" className="loginPassword" placeholder="패스워드를 입력해주세요!" />
+                        </div>
+                        <div className="loginButton">
+                            <Button className="login">로그인</Button>
+                        </div>
+                    </div>
+                    <div className="resigeterContainer">
+                        <Button>비밀번호 찾기</Button>
+                        <Button>회원가입</Button>
+                    </div>
+                    <div className="subContainer">
+                        <Button>카카오</Button>
+                        <Button>구글</Button>
+                    </div>
+                </Section>
+            <Footer/> 
+                 
+        </div>
+        );
+    }
+}
