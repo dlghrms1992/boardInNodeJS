22556242e8ba9057d744cf60cd051e018535c8dc
dev_G <dlghrms1992@naver.com>
2022-03-15 12:54:11 +0900

    비밀번호 암호화 복호화 모듈 생성 중

--- server/common/crypto.js
+++ server/common/crypto.js
@@ -1,12 +1,37 @@
-import crypto from 'crypto';
+const crypto = require('crypto');
 
-let cypto = {
+module.exports = (password) => {
+    let cryptoString = {
 
-    encrypt : function(password) {
-
-    },
-    decode : function(password){
-
+        encryptPassword : function(password) {
+            new Promise(async (resolve, reject) => {
+                const salt = await createSalt();
+                crypto.pbkdf2(password, salt, 9999, 64, 'sha512', (err, key) => {
+                    if(err) reject(err);
+                    var encryptPassword = resolve({password: key.toString('base64'), salt});
+                    return encryptPassword;
+                })
+            });
+        },
+        decodePassword : function(password){
+            const salt = await createSalt();
+            crypto.pbkdf2(password, salt, 9999, 64, 'sha512', (err, key) => {
+                if(err) reject(err);
+                var encryptPassword = resolve(key.toString('base64'));
+                return decodePassword;
+            })
+        }
+        
     }
-    
+
+    const createSalt = () => {
+        new Promise((resolve, reject) => {
+            crypto.randomBytes(64, (err, buf) => {
+                if(err) reject(err);
+                resolve(buf.toString('base64'));
+            });
+        });
+    }
+
+    return cryptoString;
 }
