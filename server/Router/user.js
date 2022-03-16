e7b108604cd76bb22046e169e1e4fe56494d7c96
dev_G <dlghrms1992@naver.com>
2022-03-16 12:44:27 +0900

    회원가입 완료

--- server/Router/user.js
+++ server/Router/user.js
@@ -1,33 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const {UserModel} = require('../model/index');
-let crypto = require('crypto');
+const crypto = require('../common/crypto');
+const {v4} = require('uuid');
 
 // const db_config = require('../../db/database.js');
 // const conn = db_config.init();
 // db_config.connect(conn);
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
     // const sql = 'insert inte toy_user values(?, ?, ?, ?, ?, ?, ?, ?)';
-    console.log("db 넣기전 테스트",req.body);
-    console.log("db 넣기전 테스트",  UserModel);
+    console.log(" call create --- >",req.body);
+
     let params = req.body;
+    const {password, salt} = await crypto.encryptPassword(params.password);
+    console.log("test ==> ",  password);
+    console.log("test ==> ",  salt);
     UserModel.create({
-        userid: crypto.randomUUID(),
+        userid: v4(),
         loginId: params.loginId,
         username: params.username,
         usernick: params.usernick,
-        password: params.password,
+        password: password,
+        password_salt: salt,
         phone: params.phone,
         email: params.email,
         type: params.type,
         regdate: new Date(),
     }).then(result => {
         console.log('result ==>', result);
-        res.send({"success": true});
+        res.send({"result": true});
     }).catch(err => {
         console.log("error ==>", err);
-        res.send({"error": err});
+        res.send({"result": err});
     });
 
 
--- server/common/crypto.js
+++ server/common/crypto.js
@@ -1,37 +1,34 @@
 const crypto = require('crypto');
 
-module.exports = (password) => {
-    let cryptoString = {
+module.exports.encryptPassword = (password) => 
+    new Promise(async (resolve, reject) => {
+        const salt = await createSalt();
+        console.log("salt === > ", salt);
+        console.log("salt === > ", password);
+        crypto.pbkdf2(password, salt, 9999, 64, 'sha512', (err, key) => {
+            if(err) reject(err);
+            resolve({password: key.toString('base64'), salt});
+        })
+    });
 
-        encryptPassword : function(password) {
-            new Promise(async (resolve, reject) => {
-                const salt = await createSalt();
-                crypto.pbkdf2(password, salt, 9999, 64, 'sha512', (err, key) => {
-                    if(err) reject(err);
-                    var encryptPassword = resolve({password: key.toString('base64'), salt});
-                    return encryptPassword;
-                })
-            });
-        },
-        decodePassword : function(password){
-            const salt = await createSalt();
-            crypto.pbkdf2(password, salt, 9999, 64, 'sha512', (err, key) => {
-                if(err) reject(err);
-                var encryptPassword = resolve(key.toString('base64'));
-                return decodePassword;
-            })
-        }
-        
-    }
 
-    const createSalt = () => {
-        new Promise((resolve, reject) => {
-            crypto.randomBytes(64, (err, buf) => {
-                if(err) reject(err);
-                resolve(buf.toString('base64'));
-            });
+module.exports.decodePassword = function(password, code){
+    crypto.pbkdf2(password, code, 9999, 64, 'sha512', (err, key) => {
+        if(err) reject(err);
+        var decodePassword = resolve(key.toString('base64'));
+        return decodePassword;
+    })
+};
+
+
+
+const createSalt = () => 
+    new Promise(async (resolve, reject) => {
+        crypto.randomBytes(64, (err, buf) => {
+            if(err) reject(err);
+            resolve(buf.toString('base64'));
         });
-    }
+    });
 
-    return cryptoString;
-}
+
+
